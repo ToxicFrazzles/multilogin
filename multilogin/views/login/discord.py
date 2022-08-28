150c837555e7d9b6968bda104ce916e23c0e5b75
Frazer <frazzles740@gmail.com>
2022-08-28 13:00:47 +0100

    Remove automatically creating users
    Replaced with saving token info to the session. Also added support for specifying the page to go to after login.

--- multilogin/views/login/discord.py
+++ multilogin/views/login/discord.py
@@ -1,7 +1,8 @@
 from django.views import View
-from django.shortcuts import redirect, render, reverse
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import redirect, render, reverse, resolve_url
 from django.contrib.auth import authenticate, login, get_user_model
-from django.utils import timezone
+from django.conf import settings
 from ...models.discord_login import DiscordLogin
 
 from ...authlib_configs import oauth
@@ -13,8 +14,7 @@ User = get_user_model()
 class DiscordLoginView(View):
     def get(self, request):
         redirect_uri = request.build_absolute_uri(reverse("multilogin:discord_redirect"))
-        if "register" in request.GET:
-            request.session["register"] = True
+        request.session["next"] = request.GET.get("next", None)
         return discord.authorize_redirect(
             request, redirect_uri
         )
@@ -22,31 +22,24 @@ class DiscordLoginView(View):
 
 class DiscordRedirectView(View):
     def get(self, request):
-        register = request.session.get("register", False)
-        request.session["register"] = False
+        next_page = request.session.get("next", None)
+        request.session["next"] = None
         token = discord.authorize_access_token(request)
         resp = discord.get("users/@me", token=token)
         resp.raise_for_status()
         resp_json = resp.json()
         discord_user_id = resp_json.get("id", None)
         if not discord_user_id:
-            return redirect("/")
+            return redirect(next_page or "/")
         user = authenticate(discord_user_id=discord_user_id)
         if user:
             login(request, user)
-            return redirect("/")
-        if register:
-            user = User(username=request.session.get("desired_username", None) or resp_json['username'])
-            user.save()
-            discord_login = DiscordLogin(
-                user=user,
-                discord_id=discord_user_id,
-                access_token=token["access_token"],
-                refresh_token=token["refresh_token"],
-                expires=timezone.datetime.fromtimestamp(token["expires_at"])
-            )
-            discord_login.save()
-            login(request, authenticate(discord_user_id=discord_user_id))
-            return redirect("/")
-        return redirect("/")
-
+            return redirect(next_page or "/")
+        request.session['oauth'] = {
+            'model': DiscordLogin,
+            'user_id': discord_user_id,
+            'access_token': token["access_token"],
+            'refresh_token': token["refresh_token"],
+            'expires': token["expires_at"]
+        }
+        return HttpResponseRedirect(resolve_url(getattr(settings, "REGISTRATION_PAGE", "/")) + f"?next={next_page}")
