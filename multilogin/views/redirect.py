66953a1561a3386004e260c21aaae92c65f0d374
Frazer <frazzles740@gmail.com>
2022-08-31 21:27:13 +0100

    Enable implicit addition of OAuth2 connections.
    If user authenticates with OAuth2 provider when already logged in, add the connection to the user and remove the password if it exists.

--- multilogin/views/redirect.py
+++ multilogin/views/redirect.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.http.response import HttpResponseRedirect
 from django.conf import settings
 from ..multilogin.main import get_user_info
+from ..models import OAuthToken
 
 
 class RedirectView(View):
@@ -16,7 +17,21 @@ class RedirectView(View):
         userinfo = get_user_info(self.backend.name, token=token)
         if not userinfo.identifier:
             return redirect(next_page)
-        user = authenticate(auth_provider=self.backend.name, identifier=userinfo.identifier)
+        if request.user.is_authenticated:
+            db_token = OAuthToken(
+                owner=request.user,
+                provider=self.backend.name,
+                identifier=userinfo.identifier,
+                **token
+            )
+            db_token.save()
+            request.user.set_unusable_password()
+            request.user.save()
+            return redirect(next_page)
+        user = authenticate(
+            auth_provider=self.backend.name,
+            identifier=userinfo.identifier
+        )
         if user:
             login(request, user)
             return redirect(next_page)
